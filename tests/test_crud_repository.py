e657a529582eb87191f2bfb08d5d042a2f050b26
William Chen <86595028+NFUChen@users.noreply.github.com>
2025-01-08 18:49:43 +0800

    refactor&bugfix: Refactor logging levels and improve CRUD repository … (#3)
    * refactor&bugfix: Refactor logging levels and improve CRUD repository methods
    
    - Updated logging levels from `info` to `debug` in `PySpringModel` for managed session operations.
    - Fixed return handling for `_find_by_query` in `CrudRepository` to properly unpack results.
    - Enhanced `delete_all` and `delete_all_by_ids` methods to handle batch deletions efficiently by selecting and deleting entities in bulk.
    - Added a comprehensive test suite (`test_crud_repository.py`) to cover CRUD repository functionalities:
      - Tests for find, delete, delete_all, delete_by_id, and upsert operations.
      - Ensures robust coverage for entity creation, updating, and removal scenarios.
    
    * add: Add test workflow yaml file

---
+++ tests/test_crud_repository.py
@@ -0,0 +1,115 @@
+import pytest
+from loguru import logger
+from sqlalchemy import create_engine
+from sqlmodel import Field, SQLModel
+
+from py_spring_model import PySpringModel
+from py_spring_model.repository.crud_repository import CrudRepository
+
+class User(PySpringModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: str
+
+class UserRepository(CrudRepository[int,User]): ...
+
+
+class TestCrudRepository:
+    def setup_method(self):
+        logger.info("Setting up test environment...")
+        self.engine = create_engine("sqlite:///:memory:", echo=True)
+        PySpringModel._engine = self.engine
+        SQLModel.metadata.create_all(self.engine)
+
+    def teardown_method(self):
+        logger.info("Tearing down test environment...")
+        SQLModel.metadata.drop_all(self.engine)
+
+    @pytest.fixture
+    def user_repository(self):
+        repo = UserRepository()
+        return repo
+
+    def create_test_user(self, user_repository: UserRepository):
+        user = User(name="John Doe", email="john@example.com")
+        user_repository.save(user)
+        logger.info(f"Created user with ID: {user.id}")
+
+    def test_did_get_model_id_type_with_class(self):
+        id_type, model_type = UserRepository._get_model_id_type_with_class()
+        assert id_type == int
+        assert model_type == User
+    
+
+    def test_find_by_id(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        user = user_repository.find_by_id(1)
+        assert user is not None
+        assert user.id == 1
+        assert user.name == "John Doe"
+
+    def test_find_all(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        users = user_repository.find_all()
+        assert len(users) == 1
+        assert users[0].id == 1
+        assert users[0].name == "John Doe"
+
+    def test_find_by_query(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        _, user = user_repository._find_by_query({"name": "John Doe"})
+        assert user is not None
+        assert user.id == 1
+        assert user.name == "John Doe"
+
+        _, email_user = user_repository._find_by_query({"email": "john@example.com"})
+        assert email_user is not None
+        assert email_user.id == 1
+        assert email_user.email == "john@example.com"
+
+    def test_delete(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        user = user_repository.find_by_id(1)
+        assert user is not None
+        assert user_repository.delete(user)
+        assert user_repository.find_by_id(1) is None
+
+    def test_delete_by_id(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        user = user_repository.find_by_id(1)
+        assert user is not None
+        assert user_repository.delete_by_id(1)
+        assert user_repository.find_by_id(1) is None
+
+    def test_delete_all(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        users = user_repository.find_all()
+        assert len(users) == 1
+        assert user_repository.delete_all(users)
+        assert len(user_repository.find_all()) == 0
+
+    def test_delete_all_by_ids(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        assert user_repository.delete_all_by_ids([1])
+        assert user_repository.find_by_id(1) is None
+    
+    def test_upsert_for_existing_user(self, user_repository: UserRepository):
+        self.create_test_user(user_repository)
+        user = user_repository.find_by_id(1)
+        assert user is not None
+        user.name = "William Chen"
+        user.email = "william.chen@example.com"
+        assert user_repository.upsert(user, {"id": 1})
+        updated_user = user_repository.find_by_id(1)
+        assert updated_user is not None
+        assert updated_user.name == "William Chen"
+        assert updated_user.email == "william.chen@example.com"
+
+
+    def test_upsert_for_new_user(self, user_repository: UserRepository):
+        user = User(name="John Doe", email="john@example.com")
+        assert user_repository.upsert(user, {"name": "John Doe"})
+        new_user = user_repository.find_by_id(1)
+        assert new_user is not None
+        assert new_user.name == "John Doe"
+        assert new_user.email == "john@example.com"
