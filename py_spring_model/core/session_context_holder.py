4e7b847321e4ceed4307f52ec6edeb4beb9a854c
William Chen <william_w_chen@trendmicro.com>
2025-06-20 02:08:51 +0800

    fix: Update SessionContextHolder to use PySpringSession

--- py_spring_model/core/session_context_holder.py
+++ py_spring_model/core/session_context_holder.py
@@ -1,7 +1,8 @@
 from contextvars import ContextVar
 from functools import wraps
 from typing import Any, Callable, ClassVar, Optional
-from sqlmodel import Session
+
+from py_spring_model.core.py_spring_session import PySpringSession
 
 from py_spring_model.core.model import PySpringModel
 
@@ -67,9 +68,9 @@ class SessionContextHolder:
     This is useful for the query service to be able to access the session without having to pass it in as an argument.
     This is also useful for the query service to be able to access the session without having to pass it in as an argument.
     """
-    _session: ClassVar[ContextVar[Optional[Session]]] = ContextVar("session", default=None)
+    _session: ClassVar[ContextVar[Optional[PySpringSession]]] = ContextVar("session", default=None)
     @classmethod
-    def get_or_create_session(cls) -> Session:
+    def get_or_create_session(cls) -> PySpringSession:
         optional_session = cls._session.get()
         if optional_session is None:
             session = PySpringModel.create_session()
