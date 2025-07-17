4f59f861c64757320c6a373cd90c3dc9f19093e5
William Chen <william_w_chen@trendmicro.com>
2025-07-17 20:03:03 +0800

    feat: Add versioning and export Query in PySpringModel
    - Introduced __version__ attribute to specify the current version of the package.
    - Updated __all__ to include Query for better module accessibility.

--- py_spring_model/__init__.py
+++ py_spring_model/__init__.py
@@ -5,3 +5,17 @@ from py_spring_model.repository.crud_repository import CrudRepository
 from py_spring_model.repository.repository_base import RepositoryBase
 from py_spring_model.py_spring_model_rest.service.curd_repository_implementation_service.crud_repository_implementation_service import SkipAutoImplmentation
 from py_spring_model.py_spring_model_rest.service.query_service.query import Query
+
+
+__all__ = [
+    "PySpringModel",
+    "Field",
+    "SessionContextHolder",
+    "provide_py_spring_model",
+    "CrudRepository",
+    "RepositoryBase",
+    "SkipAutoImplmentation",
+    "Query",
+]
+
+__version__ = "0.1.0"
